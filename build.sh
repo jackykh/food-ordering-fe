1df38a2378873dcda51f61b21609c04076971a14
Jacky Cheung <cheungkwingho@gmail.com>
2025-10-18 22:00:13 +0800

    update build script

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
     # Run the "build" script in `package.json` on the "main" branch
 
-  npm run build
+  VITE_API_BASE_URL=$VITE_API_BASE_URL npm run build
 
 elif [ "$CF_PAGES_BRANCH" == "preview" ]; then
     # Run the "build" script in `package.json` on the "preview" branch
 
-  npm run build
+  VITE_API_BASE_URL=$VITE_API_BASE_URL npm run build
 
 else
     # Run the "build:dev" script in `package.json` on other branches
